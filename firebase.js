1864d3ccf108deb3f5850a6339cf2acf4ac5ee63
czviewer <czmypcs@gmail.com>
2025-07-04 14:04:54 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
-
+// firebase.js (for use via <script src>)
 const firebaseConfig = {
   apiKey: "AIzaSyAyhQ_u0aj_TDUQGC0J_Jcq5kzVmIlJrNQ",
   authDomain: "diya-motors-tracker.firebaseapp.com",
   databaseURL: "https://diya-motors-tracker-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "diya-motors-tracker",
-  storageBucket: "diya-motors-tracker.firebasestorage.app",
+  storageBucket: "diya-motors-tracker.appspot.com",
   messagingSenderId: "863309961024",
   appId: "1:863309961024:web:000cec3cdfe97aaaf60ca5",
   measurementId: "G-PJK0E9410H"
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getDatabase(app);
-
-
-
+// Load compat version
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const auth = firebase.auth();
